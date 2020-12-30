872dcc80b81fa7b878cee5fc13374ff844b6f0b0
Michael Bumann <hello@michaelbumann.com>
2020-12-30 02:21:59 +0100

    Add logging when we launch a window

--- executor.js
+++ executor.js
@@ -93,6 +93,7 @@ module.exports = class Executor {
   }
 
   launchWindow(route, message) {
+    console.log(`Launching ${route}`);
     return this.app.whenReady().then(() => {
       this.window = new BrowserWindow({
         width: 800,
